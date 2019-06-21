b240eed4824721fc506f61ae8d5efe25fdc9e693
Gustavo Graeff <gustavograeff1998@gmail.com>
2019-06-21 15:04:24 -0300

    fiz router and data bug

--- vueOrigamidClasses/src/App.vue
+++ vueOrigamidClasses/src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+
+  </div>
 </template>
 
 <script>
@@ -9,8 +11,10 @@ export default {
 
   },
 
-  data: {
+  data () {
+    return {
 
+    }
   },
   funtion: {
 
