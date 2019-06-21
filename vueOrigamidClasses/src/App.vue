f92c2ffd6347346cbe18a7abf2e5d714771f46ba
Gustavo Graeff <gustavograeff1998@gmail.com>
2019-06-21 16:54:24 -0300

    add exercise from class 0106

--- vueOrigamidClasses/src/App.vue
+++ vueOrigamidClasses/src/App.vue
@@ -1,6 +1,15 @@
 <template>
-  <div id="app">
-
+  <div id="app" :style="{background: colorSelected}">
+    <h1>Seide: {{side}}</h1>
+    <h1>Square Area: {{side * side}}</h1>
+    <a v-bind:href="href">{{href}}</a>
+    <p></p>
+    <input v-model="text" type="text">
+    <p></p>
+    {{text}}
+    <p></p>
+    <input v-model="colorSelected" type="color">
+    <p></p>
   </div>
 </template>
 
@@ -13,7 +22,10 @@ export default {
 
   data () {
     return {
-
+      side: 5,
+      href: 'https://github.com/ggraeff98',
+      text: '',
+      colorSelected: '#333'
     }
   },
   funtion: {
