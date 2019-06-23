3f2e84c0e29ce4c58324e8b2b1c25cba52a88912
Gustavo Graeff <gustavograeff1998@gmail.com>
2019-06-23 16:50:10 -0300

    add exercise from class 0204

--- vueOrigamidClasses/src/App.vue
+++ vueOrigamidClasses/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-
+    <button @click="pullData">Pull Data</button>
+    <div :class="acoes.marketCap[0] > acoes.marketCap[1] ? 'highMarketCap' : 'lowerMarketCap'" ref="showCompany1"></div>
+    <div :class="acoes.marketCap[0] > acoes.marketCap[1] ? 'highMarketCap' : 'lowerMarketCap'" ref="showMarketCap1"></div>
+    <div :class="acoes.marketCap[0] < acoes.marketCap[1] ? 'highMarketCap' : 'lowerMarketCap'" ref="showCompany2"></div>
+    <div :class="acoes.marketCap[0] < acoes.marketCap[1] ? 'highMarketCap' : 'lowerMarketCap'" ref="showMarketCap2"></div>
   </div>
 </template>
 
@@ -13,9 +17,22 @@ export default {
 
   data () {
     return {
-
+      acoes: {
+        companyName: ['Google', 'Apple'],
+        marketCap: [1000, 2000]
+      }
     }
   },
+
+  methods: {
+    pullData () {
+      this.$refs.showCompany1.innerHTML = this.acoes.companyName[0]
+      this.$refs.showMarketCap1.innerHTML = this.acoes.marketCap[0]
+      this.$refs.showCompany2.innerHTML = this.acoes.companyName[1]
+      this.$refs.showMarketCap2.innerHTML = this.acoes.marketCap[1]
+    }
+  },
+
   funtion: {
 
   }
@@ -23,5 +40,11 @@ export default {
 </script>
 
 <style>
+.highMarketCap {
+  color: green;
+}
 
+.lowerMarketCap {
+  color: red;
+}
 </style>
