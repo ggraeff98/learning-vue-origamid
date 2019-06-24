ec55581922325f0fc0b6de55ec61bd10606e944f
Gustavo Graeff <gustavograeff1998@gmail.com>
2019-06-24 17:58:24 -0300

    feat(App):add answer from exercise 0206

--- vueOrigamidClasses/src/App.vue
+++ vueOrigamidClasses/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-
+    <ul>
+    <li @click.prevent="pullActions"><a href="http://localhost:3000/google">Google</a></li>
+    <li @click.prevent="pullActions"><a href="http://localhost:3000/apple">Apple</a></li>
+  </ul>
+  <div>
+    <p>Name: {{action.name}}</p>
+  </div>
   </div>
 </template>
 
@@ -13,11 +19,22 @@ export default {
 
   data () {
     return {
-
+      action: {}
     }
   },
   funtion: {
 
+  },
+  methods: {
+    pullActions (event) {
+      const link = (event.target.href)
+      fetch(link)
+        .then(answer => answer.json())
+        .then(answer => {
+          this.action = answer[0]
+          console.log(answer[0])
+        })
+    }
   }
 }
 </script>
