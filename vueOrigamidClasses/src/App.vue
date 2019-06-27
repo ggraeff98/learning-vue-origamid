2029f76bb06919e7b20df98f639ee10e620dc47e
Gustavo Graeff <gustavograeff1998@gmail.com>
2019-06-27 02:13:33 -0300

    add class 0207

--- vueOrigamidClasses/src/App.vue
+++ vueOrigamidClasses/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-
+    <ul>
+      <li v-for="(item, index) in lista" :key="index">{{item}}</li>
+    </ul>
+    <button @click="removeItem">Romove Item</button>
+    <button @click="change">Change</button>
   </div>
 </template>
 
@@ -13,14 +17,18 @@ export default {
 
   data () {
     return {
-
+      lista: ["Item 1", "Item 2", "Item 3"],
     }
   },
   funtion: {
-
   },
   methods: {
-
+    removeItem () {
+      this.lista.pop();
+    },
+    change () {
+      this.$set(this.lista, 0, "new Item")
+    }
   }
 }
 </script>
