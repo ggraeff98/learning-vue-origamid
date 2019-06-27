9d923fa43abe96437a9897c9b81fd046999a2065
Gustavo Graeff <gustavograeff1998@gmail.com>
2019-06-27 01:02:53 -0300

    add exercise from class 0206

--- vueOrigamidClasses/src/App.vue
+++ vueOrigamidClasses/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-
+    <button @click="addItens()">Add Item</button>
+    <button @click="removeItens()">Remove Item</button>
+    <p>Total: {{totalItens}}</p>
+    <p>No Carrinho: {{itensNoCarrinho}}</p>
+    <p v-if="totalItens < 1">Não se pode adicionar mais itens</p>
   </div>
 </template>
 
@@ -13,11 +17,28 @@ export default {
 
   data () {
     return {
-
+      totalItens: 7,
+      itensNoCarrinho: 0
     }
   },
   funtion: {
 
+  },
+  methods: {
+    addItens () {
+      if (this.totalItens > 0) {
+        this.totalItens--
+        this.itensNoCarrinho++
+        console.log(this.totalItens)
+      }
+    },
+    removeItens () {
+      if (this.itensNoCarrinho > 0) {
+        this.totalItens++
+        this.itensNoCarrinho--
+        console.log(this.totalItens)
+      }
+    }
   }
 }
 </script>
