259d71f94db12ae4a9a1524327eaefbf29075dc6
Gustavo Graeff <gustavograeff1998@gmail.com>
2019-06-28 01:47:29 -0300

    ad exercise from class 0209-2

--- vueOrigamidClasses/src/App.vue
+++ vueOrigamidClasses/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-
+    <p>{{allName}}</p>
+    <button @click="pullCountries">Pull Countries</button>
+    <select name="paises" id="paises" v-model="selected">
+      <option v-for="(country, index) in countries" :key="index" :value="country.name">{{country.name}}</option>
+    </select>
+    <p>{{countryToFindCapital.capital}}</p>
   </div>
 </template>
 
@@ -13,17 +18,35 @@ export default {
 
   data () {
     return {
-
+      name: 'Gustavo',
+      lastName: 'Graeff',
+      selected: '',
+      countries: {},
+      capital: '',
+      countryToFindCapital: {}
     }
   },
   funtion: {
 
   },
   methods: {
-
+    pullCountries () {
+      fetch('https://restcountries.eu/rest/v2/all')
+        .then(recive => recive.json())
+        .then(recive => {
+          this.countries = recive
+        })
+    }
   },
   computed: {
-
+    allName () {
+      return this.name + ' ' + this.lastName
+    }
+  },
+  watch: {
+    selected (countrySelected) {
+      this.countryToFindCapital = this.countries.find((country) => country.name === countrySelected)
+    }
   }
 }
 </script>
