67b0fa5fd5e8ddf22b7ba7215964f3c978d91960
Gustavo Graeff <gustavograeff1998@gmail.com>
2019-06-30 11:54:01 -0300

    add exercise from class 0302-2

--- vueOrigamidClasses/src/App.vue
+++ vueOrigamidClasses/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-
+    <button @click="inverter">Inverter Lista</button>
+    <ul @click="trocaCor" :class="{column : inverte}">
+      <li :style="{background : color}">Sobre</li>
+      <li :style="{background : color}">Produtos</li>
+      <li :style="{background : color}">Serviços</li>
+      <li :style="{background : color}">Contato</li>
+    </ul>
   </div>
 </template>
 
@@ -13,13 +19,20 @@ export default {
 
   data () {
     return {
-
+      inverte: false,
+      color: '#84e'
     }
   },
   funtion: {
 
   },
   methods: {
+    inverter () {
+      this.inverte = !this.inverte
+    },
+    trocaCor () {
+      this.color = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }
 
   },
   computed: {
@@ -29,5 +42,24 @@ export default {
 </script>
 
 <style>
-
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    font-family: monospace;
+    list-style: none;
+    padding: 0px;
+    margin: 0px;
+  }
+  .column {
+    flex-direction: column;
+  }
+  li {
+    padding: 20px;
+    background: #84e;
+    color: white;
+    border-radius: 4px;
+    margin: 10px;
+    flex: 1;
+    text-align: center;
+  }
 </style>
