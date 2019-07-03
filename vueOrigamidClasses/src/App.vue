31912f37538104db71071c2c53de3a0cd18dab71
Gustavo Graeff <gustavograeff1998@gmail.com>
2019-07-03 01:20:59 -0300

    add exercise from class 0303-2

--- vueOrigamidClasses/src/App.vue
+++ vueOrigamidClasses/src/App.vue
@@ -1,6 +1,37 @@
 <template>
   <div id="app">
-
+    <input type="text" v-model="buttonStyle.width"/>
+    <br>
+    <input type="text" v-model="buttonStyle.height"/>
+    <br>
+    <input type="text" v-model="buttonStyle.fontSize"/>
+    <br>
+    <input type="text" v-model="buttonStyle.fontFamily"/>
+    <br>
+    <input type="text" v-model="buttonStyle.textAlign"/>
+    <br>
+    Back Groung:
+    <input type="color" v-model="buttonStyle.backgroundColor"/>
+    <br>
+    Text Color:
+    <input type="color" v-model="buttonStyle.color"/>
+    <br>
+    <input type="text" v-model="buttonStyle.borderRadius"/>
+    <br>
+    <button :style="buttonStyle">Click</button>
+    <br><br><br><br>
+    <input type="text" v-model="person.name" placeholder="name"/>
+    <br>
+    <input type="text" v-model="person.email"/>
+    <br>
+    <input type="text" v-model="person.telefone"/>
+    <br>
+    <input type="text" v-model="person.mensage"/>
+    <button @click.prevent="load">Load</button>
+    <button @click="reset">Reset</button>
+    <span v-if="showPersonInfos">
+      {{person.name}}, {{person.email}}, {{person.telefone}}, {{person.mensage}}
+    </span>
   </div>
 </template>
 
@@ -10,19 +41,41 @@ export default {
   components: {
 
   },
-
   data () {
     return {
-
+      buttonStyle: {
+        width: '',
+        height: '40px',
+        fontSize: '10pt',
+        fontFamily: '',
+        textAlign: 'center',
+        backgroundColor: '',
+        color: '',
+        borderRadius: '50%'
+      },
+      person: {
+        name: '',
+        email: '',
+        temlefone: '996090393',
+        mensage: ''
+      },
+      showPersonInfos: false
     }
   },
   funtion: {
 
   },
   methods: {
-
+    load () {
+      this.showPersonInfos = true
+    },
+    reset () {
+      this.showPersonInfos = false
+    }
   },
   computed: {
+  },
+  watch: {
 
   }
 }
