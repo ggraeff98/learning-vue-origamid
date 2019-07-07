7311475cf85daf578635a483be25deef7ed15cc9
Gustavo Graeff <gustavograeff1998@gmail.com>
2019-07-07 15:45:13 -0300

    add exercise and notes from class 0305-2

--- vueOrigamidClasses/src/App.vue
+++ vueOrigamidClasses/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-
+    {{cont}}
+    <button @click="cont++">cont+</button>
+    <ul>
+      <li v-for="(value, key) in githubProfileInfos" :key="key">{{key}} : {{value}}</li>
+    </ul>
   </div>
 </template>
 
@@ -13,18 +17,33 @@ export default {
 
   data () {
     return {
-
+      githubProfileInfos: {},
+      cont: 0
     }
   },
   funtion: {
 
   },
   methods: {
-
+    test () {
+      fetch('https://api.github.com/users/ggraeff98')
+        .then(recive => recive.json())
+        .then(recive => {
+          this.githubProfileInfos = recive
+          console.log(this.githubProfileInfos)
+        })
+    }
   },
   computed: {
 
+  },
+  created () {
+    this.test()
+  },
+  updated () {
+    document.title = this.cont
   }
+
 }
 </script>
 
